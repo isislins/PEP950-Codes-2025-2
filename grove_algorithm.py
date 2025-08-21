09e7cc1cb3f88d757cf783ff4d2e4f18ae8b3d5f
Gustavo C R Lima <gugacamargorochalima@gmail.com>
2025-08-21 18:32:27 -0300

    ADD grover with 3+ qubits

--- grove_algorithm.py
+++ grove_algorithm.py
@@ -4,31 +4,30 @@ from qiskit_aer import Aer
 from qiskit import transpile
 
 # Create a new circuit with three qubits and three classical bits
-qc = QuantumCircuit(2, 2)
+qc = QuantumCircuit(3, 3)
 
 # --- State Preparation ---
-qc.h(0)
-qc.h(1)
+qc.h([0,1,2])
 # com ambos os qubits em superposição, temos |00> + |01> + |10> + |11>
-# --- Oracle (|10> marked em bits isso é 01 pois os qubits são invertidos na medição) ---
-qc.x([1]) # Troco o 0 por 1
+# --- Oracle (|010> marked em bits isso é 101 pois os qubits são invertidos na medição) ---
+qc.x([0,2]) # Troco o 0 por 1
 
-## porta CZ
-qc.h(1) # coloco em superposição o último qubit
-qc.cx(0, 1) # Inverte o sinal do estado |11> (A ideia é que o cx entre duas portas H é um CZ, e o CZ inverte o sinal do estado |111...1>)
-qc.h(1)
+## porta CZ para inverter a fase do estado marcado
+qc.h(2) # coloco em superposição o último qubit
+qc.mcx([0, 1],2) # Inverte o sinal do estado |111> (A ideia é que o cx entre duas portas H é um CZ, e o CZ inverte o sinal do estado |111...1>)
+qc.h(2)
 ## fim da porta CZ
-qc.x([1]) # volto o último qubit para |0>
-# inversão de fase para o estado marcado
+qc.x([0,2]) # retorno os qubits trocados na entrada do oráculo para o estad
+
 
 # --- Grover Operator ---
-qc.h([0, 1])
-qc.x([0, 1])
-qc.h(1)
-qc.cx(0, 1)
-qc.h(1)
-qc.x([0,1])
-qc.h([0,1])
+qc.h([0, 1, 2])
+qc.x([0, 1, 2])
+qc.h(2)
+qc.mcx([0, 1], 2)
+qc.h(2)
+qc.x([0,1,2])
+qc.h([0,1,2])
 
 
 # Visualizar o circuito
@@ -36,7 +35,7 @@ print(qc.draw('text'))
 
 
 # --- Measurement ---
-qc.measure([0, 1], [0, 1])
+qc.measure([0, 1, 2], [0, 1, 2])
 
 # Para obter o statevector:
 state_simulator = Aer.get_backend('statevector_simulator')
